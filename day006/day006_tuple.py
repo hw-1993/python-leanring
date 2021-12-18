c30a42f299c0af1c8d42d27309264250f719e458
hw-1993 <developer_hw@foxmail.com>
2021-12-18 23:52:53 +0800

    python tuple learning.

---
+++ day006/day006_tuple.py
@@ -0,0 +1,7 @@
+if __name__ == '__main__':
+    immutable_list = ('hello', 'world',)
+    print(immutable_list)
+    print(len(immutable_list))
+    single_immutable_list = ('single',)
+    print(single_immutable_list)
+    print(len(single_immutable_list))
